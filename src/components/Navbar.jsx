5e86ad16e3de9be58ec3bf3cd80f7949c0fa98f0
z.elqarch <zakariaelqarch@gmail.com>
2024-01-30 17:06:16 +0100

    navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20
+    bg-primary`}
+    >
+      <div
+        className="w-full flex justify-between items-center
+       max-w-7xl mx-auto"
+      >
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Zakaria <span className="sm:block hidden">| El Qarch</span>{" "}
+          </p>
+        </Link>
+        <ul className="list-none hiden sm:flex fles-row gap-10">
+          {navLinks.map((link) => {
+            return (
+              <li key={link.id}
+              className={`${active ===link.title? "text-white" :"text-secondary"}
+              hover:text-white text-[18px] font-medium cursor-pointer`}>
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
